9f29b58b507b269e0e7372d6afd41398e54fba2e
sadboi <tomassestak@seznam.cz>
2023-03-13 11:45:49 +0100

    add await

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+
+  const tryFaceId = async () => {
+    const result = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Log in with FaceID",
+      disableDeviceFallback: true,
+      cancelLabel: 'Cancel',
+    });
+
+    alert(result.success ? 'Success' : result.error)
+
+    console.log(result)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title={'Try FaceID'} onPress={tryFaceId} />
       <StatusBar style="auto" />
     </View>
   );
